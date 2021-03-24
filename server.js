e74a656aadfb2eda68ff4bf51cdf9a99f7528096
samira-estes-garcia <samira.estes.garcia@gmail.com>
2021-03-24 14:01:38 -0500

    reconfig server.js added app folder

--- server.js
+++ server.js
@@ -2,19 +2,39 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const url = 'mongodb+srv://admin:admin@cluster0.rdq0o.mongodb.net/todo_mongoose_react?retryWrites=true&w=majority';
 
-mongoose.connect(url, { useNewUrlParser: true }, { useUnifiedTopology: true });
+let corsOptions = {
+    origin: "http://localhost:8081"
+};
 
-const db = mongoose.connection
-db.once('open', _ => {
-    console.log('database connected:', url);
-});
+app.use(cors(corsOptions));
 
-db.on('error', err => {
-    console.error('connection error:', err);
+//parse requests of content-typ: application/json
+app.use(express.json());
+
+//parse requests of content-type: application/x-www-form-urlencoded
+app.use(express.urlencoded( { extended: true }));
+
+//get route
+app.get('/', (req, res) => {
+    res.json({ message: "Welcome to To Do App"});
 })
 
-app.listen(3000, function() {
-    console.log('listening on port 3000');
+//set port and listen for requests
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
 });
+
+// mongoose.connect(url, { useNewUrlParser: true }, { useUnifiedTopology: true });
+
+// const db = mongoose.connection
+// db.once('open', _ => {
+//     console.log('database connected:', url);
+// });
+
+// db.on('error', err => {
+//     console.error('connection error:', err);
+// })
